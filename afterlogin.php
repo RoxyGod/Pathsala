9e0cde8e0577a51ca71733682685bb4aff3bf8c8
RoxyGod <97722410+RoxyGod@users.noreply.github.com>
2022-01-31 00:19:12 +0545

    Update afterlogin.php

--- afterlogin.php
+++ afterlogin.php
@@ -1,26 +1,75 @@
-<!doctype html>
-<html lang="en">
-
-<head>
+<html>
+    <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
-  <link rel="stylesheet" href="css/innerstyle.css">
   <link rel="stylesheet" text="text/css"
     href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
   <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;600&display=swap" rel="stylesheet">
-
+  
   <title>Khulla Pathsala </title>
+  <body>
+
+  <style>
+body {
+  margin: 0;
+  font-family: "Lato", sans-serif;
+}
+
+.sidebar {
+  margin: 0;
+  padding: 0;
+  width: 200px;
+  background-color: #f1f1f1;
+  position: fixed;
+  height: 100%;
+  overflow: auto;
+}
+
+.sidebar a {
+  display: block;
+  color: black;
+  padding: 16px;
+  text-decoration: none;
+}
+ 
+.sidebar a.active {
+  background-color: orange;
+  color: white;
+}
+
+.sidebar a:hover:not(.active) {
+  background-color: #555;
+  color: white;
+}
+
+div.content {
+  margin-left: 200px;
+  padding: 1px 16px;
+  height: 1000px;
+}
+
+@media screen and (max-width: 700px) {
+  .sidebar {
+    width: 100%;
+    height: auto;
+    position: relative;
+  }
+  .sidebar a {float: left;}
+  div.content {margin-left: 0;}
+}
+
+@media screen and (max-width: 400px) {
+  .sidebar a {
+    text-align: center;
+    float: none;
+  }
+}
+</style>
 </head>
-
-
-<body>
-</head>
-<body>
-
 <div class="sidebar">
-  <a class="active" href="#">Dashboard</a>
-  <a href="#">Course</a>
+  <a class="active"href="#">Dashboard</a>
+  <a  href="#">Course</a>
   <a href="#"> Video Materials</a>
   <a href="#">E-library</a>
   <a href="#">Quiz</a>
@@ -28,8 +77,7 @@
   <a href="#">Review</a>
   <a href="#">logout</a>
 </div>
-
-
-
+<div class="content">
+  <!--write-->
 </body>
 </html>
